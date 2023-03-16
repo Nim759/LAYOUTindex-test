0ef714acd9046de34563aa035d78d4c1232fbddb
Nimesha Lakshani <lnimesha759@gmail.com>
2023-03-17 00:57:37 +0530

    created add location form

--- src/Pages/addlocation/addlocation.js
+++ src/Pages/addlocation/addlocation.js
@@ -1,9 +1,98 @@
 import React from 'react'
+import { useState } from 'react';
+import FormInput from '../../Components/Forminput/FormInput';
 
-const addlocation = () => {
+const Addlocation = () => {
+
+  const [values, setValues] = useState({
+    name: "",
+    Address:"",
+    phone:"",
+   
+   
+  });
+
+const inputs =[
+ {
+    id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "Location Name",
+      errorMessage:
+        "Provide a location name",
+      label: "Location Name",
+      required: true,
+      unique: true,
+ },
+
+ 
+ {
+  id: 2,
+    name: "address",
+    type: "text",
+    placeholder: "Address",
+    errorMessage:
+      "Provide the address!",
+    label: "Address",
+   
+}
+,
+ {
+      id: 3,
+      name: "phone",
+      type: "phone",
+      placeholder: "Phone Number",
+      errorMessage: "Provide valid Phone number!",
+      label: "Phone Number",
+      pattern:"^{10}$"
+    
+ }
+ 
+
+];
+
+
+
+
+
+const onChange = (e) => {
+  console.log(e);
+  let name = e.target.name;
+  let value = e.target.value;
+  setValues({ ...values, [name]:value});
+  
+}
+
+const handleSubmit = async(e) => {
+  e.preventDefault();
+
+  const { 
+    name,
+    address,
+    phone
+    } = values
+
+  
+};
+  
   return (
-    <div>addlocation</div>
+    <div className='container'>
+      <form className= "locationform">
+        <h1>ADD LOACTION</h1>
+        {inputs.map((input)=>(
+          <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+              />
+        ))}
+
+        <button className='next'onClick={handleSubmit}>NEXT</button>
+
+      </form>
+    </div>
   )
 }
 
-export default addlocation
+export default Addlocation
